548103a8180ff3540ab450a7b39536c431eb51e1
cyk5w84cyw <jhentle.anamong@gmail.com>
2017-05-03 03:51:44 -0700

    Watched and implemented lessons 1 to 6
    Lessons:
    1 - Initial Database Setup With Seeding
    2 - Test-Driving Threads
    3 - A Thread Can Have Replies
    4 - A User May Respond to Threads
    5 - The Reply Form
    6 - A User May Publish Threads

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+Route::get('/threads', 'ThreadsController@index');
+Route::post('/threads', 'ThreadsController@store');
+Route::get('/threads/{thread}', 'ThreadsController@show')->name('threads.show');
+Route::post('/threads/{thread}/replies', 'RepliesController@store');
