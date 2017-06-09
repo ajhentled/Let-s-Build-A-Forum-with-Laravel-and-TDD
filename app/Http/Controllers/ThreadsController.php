bdc5c8a6f1e0c3d0f2ed9a7f87534965ecfa77fb
Jhentle Anamong <ajhentled@gmail.com>
2017-06-09 03:50:42 -0700

    Implement and watch lessons 16 to 19
    Lessons:
    16 - Meta Information and Pagination
    17 - A User Can Filter Threads By Popularity
    18 - A User Can Favorite Replies
    19 - The Favorite Button

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -29,6 +29,10 @@ class ThreadsController extends Controller
     {
         $threads = $this->getThreads($channel, $filters);
 
+        if (request()->wantsJson()) {
+            return $threads;
+        }
+
         return view('threads.index', compact('threads'));
     }
 
@@ -75,7 +79,10 @@ class ThreadsController extends Controller
      */
     public function show($channelId, Thread $thread)
     {
-        return view('threads.show', compact('thread'));
+        return view('threads.show', [
+            'thread' => $thread,
+            'replies' => $thread->replies()->paginate(20)
+        ]);
     }
 
     /**
@@ -92,6 +99,8 @@ class ThreadsController extends Controller
         if ($channel->exists) {
             $threads->where('channel_id', $channel->id);
         }
+
+        // dd($threads->toSql());
 
         return $threads->get();
     }
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -10,10 +10,13 @@
                 <div class="panel-body">
                     @foreach ($threads as $thread)
                         <article>
-                            <h4>
-                                <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                                {{-- <a href="{{ route('threads.show', $thread) }}">{{ $thread->title }}</a> --}}
-                            </h4>
+                            <div class="level">
+                                <h4 class="flex">
+                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+                                    {{-- <a href="{{ route('threads.show', $thread) }}">{{ $thread->title }}</a> --}}
+                                </h4>
+                                <a href="{{ $thread->path() }}">{{ $thread->replies_count }}</a>
+                            </div>
                             <div class="body">{{ $thread->body }}</div>
                         </article>
                         <hr>
